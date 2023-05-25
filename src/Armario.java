3767dd8018ab1e275556e1d51b8be866b9115f1f
valentin9nogar@gmail.com <valentin9nogar@gmail.com>
2023-05-24 22:26:10 -0300

    Cree la clase usuario y puse las variables de clase

--- src/Armario.java
+++ src/Armario.java
@@ -1,8 +1,4 @@
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,10 +11,11 @@ public class Armario extends JPanel {
     private JButton closeButton;
     private List<String> skins;
     private int indiceActual;
+    JLabel printSkin;
     private JLabel jLabelSkin;
 
     public Armario(JPanel panelPrincipal, CardLayout cardLayout) {
-        skins = new ArrayList<>();
+        skins = new ArrayList<String>();
         skins.add("Skin 1");
         skins.add("Skin 2");
         skins.add("Skin 3");
@@ -29,54 +26,66 @@ public class Armario extends JPanel {
         closeButton.setFocusPainted(false);
         closeButton.setBackground(Color.MAGENTA.darker());
 
+        jLabelSkin = new JLabel(skins.get(indiceActual));
+
         // Configurar la ventana
-        setLayout(new FlowLayout());
+        setLayout(new BorderLayout());
         setPreferredSize(new Dimension(800, 600));
 
         sigSkin = new JButton("Siguiente");
         sigSkin.setBorderPainted(false);
         sigSkin.setFocusPainted(false);
-        sigSkin.setBackground(Color.blue.darker());
+        sigSkin.setBackground(Color.blue);
 
-        antSkin = new JButton("Anterio");
+        antSkin = new JButton("Anterior");
         antSkin.setBorderPainted(false);
         antSkin.setFocusPainted(false);
-        antSkin.setBackground(Color.blue.darker());
+        antSkin.setBackground(Color.blue);
 
-        add(sigSkin,BorderLayout.NORTH);
-        add(antSkin,BorderLayout.NORTH);
-        add(closeButton,BorderLayout.NORTH);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(antSkin);
+        buttonPanel.add(sigSkin);
+        buttonPanel.add(closeButton);
+
+        add(buttonPanel, BorderLayout.NORTH);
+        add(jLabelSkin, BorderLayout.WEST);
 
         sigSkin.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {recorrerSiguienteSkin();}
+            public void actionPerformed(ActionEvent e) {
+                recorrerSiguienteSkin();
+            }
         });
 
         antSkin.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {recorrerAnteriorSkin();}
+            public void actionPerformed(ActionEvent e) {
+                recorrerAnteriorSkin();
+            }
         });
+
         closeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 cardLayout.show(panelPrincipal, "menu");
             }
         });
-
     }
 
     private void recorrerSiguienteSkin() {
         if (indiceActual < skins.size() - 1) {
             indiceActual++;
-            jLabelSkin.setText(skins.get(indiceActual));
+        } else {
+            indiceActual = 0; // Volver al principio si alcanza el final de la lista
         }
+        jLabelSkin.setText(skins.get(indiceActual));
     }
 
     private void recorrerAnteriorSkin() {
         if (indiceActual > 0) {
             indiceActual--;
-            jLabelSkin.setText(skins.get(indiceActual));
+        } else {
+            indiceActual = skins.size() - 1; // Ir al final de la lista si alcanza el principio
         }
+        jLabelSkin.setText(skins.get(indiceActual));
     }
-
 }
-
---
+++ src/Usuario.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Usuario {
+    private List<String> skins;
+    int xp;
+    int nivel;
+    int coins;
+    int horasJugadas;
+    int torneosJugados;
+
+
+}
